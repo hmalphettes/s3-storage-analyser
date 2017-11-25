3b0cf6726120ef7d7af4a362bae539b60ea7ebec
hmalphettes <hmalphettes@gmail.com>
2017-11-25 21:56:08 +0800

    REST API

--- integration/dockerhub_wh_listener.py
+++ integration/dockerhub_wh_listener.py
@@ -4,11 +4,93 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
 from pprint import pprint
 import subprocess
+import threading
 import os
 import json
 
+# Run a single onbuild or analysis at a time
+LOCK_ONBUILD = threading.Lock()
+LOCK_ANALYSIS = threading.Lock()
+
 class RequestHandler(BaseHTTPRequestHandler):
-    """Server for handlng docker hub webhook"""
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+    def do_GET(self):
+        if 'ico' in self.path:
+            self.send_response(404)
+            self.end_headers()
+            return
+
+        token = os.environ['TOKEN'] or 's3cr3t'
+
+        query_components = {
+            'token': None,
+            'unit': None,
+            'prefix': None,
+            'conc': None,
+            'fmt': None,
+            'pretty': None
+        }
+        query = urlparse(self.path).query
+        if query:
+            query_components.update(dict(qc.split("=") for qc in query.split('&')))
+
+        if query_components['token'] != token:
+            # redirect to github if the token is missing
+            self.send_response(302)
+            self.send_header('Location', 'https://github.com/hmalphettes/s3-storage-analyser')
+            self.end_headers()
+            return
+
+        unit = query_components['unit']
+        prefix = query_components['prefix']
+        conc = query_components['conc']
+        fmt = query_components['fmt']
+        echo = 'echo' in query_components
+        if fmt is None:
+            accept = self.headers['Accept'] if 'Accept' in self.headers else ''
+            print(f'accept: {accept}')
+            if query_components.get('pretty'):
+                fmt = 'json_pretty'
+            elif 'json' in accept:
+                fmt = 'json'
+            elif 'csv' in accept:
+                fmt = 'csv'
+            elif 'tab-separated-values' in accept:
+                fmt = 'tsv'
+            elif 'text/plain' in accept:
+                fmt = 'plain'
+            elif 'html' in accept:
+                fmt = 'html'
+            else:
+                fmt = 'json'
+
+        # sanitize before we call docker
+        for param in [unit, prefix, conc, fmt]:
+            if param is None:
+                continue
+            if ' ' in param or '\t' in param or len(param) > 32:
+                self.send_response(401)
+                self.end_headers()
+                return
+
+        try:
+            out = _run_analysis(unit=unit, prefix=prefix, conc=conc, fmt=fmt, echo=echo)
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(out)
+        except Exception as err:
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write(err.__str__().encode())
+        return
+
+
+    """Server for handling docker hub webhook"""
     def do_POST(self):
         """
         Expect a JSON payload of the format described here:
@@ -34,32 +116,75 @@ class RequestHandler(BaseHTTPRequestHandler):
         """
         content_len = int(self.headers['content-length'])
         post_body = self.rfile.read(content_len)
-        data = json.loads(post_body)
 
         try:
+            data = json.loads(post_body)
             tag = data['push_data']['tag']
             repo_name = data['repository']['repo_name']
             if repo_name != 'test':
                 # Security: Dont allow our service to be used for any kind of docker images!
                 repo_name = 'hmalphettes/s3-storage-analyser'
-            out = _run_cmd(repo_name, tag)
+            out = _run_onbuild(repo_name, tag)
             self.send_response(200)
             self.end_headers()
             self.wfile.write(out)
-        except subprocess.CalledProcessError as err:
+        except json.decoder.JSONDecodeError as err:
+            print(f'Unable to parse {post_body}')
             self.send_response(500)
             self.end_headers()
             self.wfile.write(err.__str__())
+        except subprocess.CalledProcessError as err:
+            print(f'Subprocess Error {err.__str__().encode()}')
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write(err.__str__().encode())
+        except Exception as err:
+            print(f'Error {err.__str__()}')
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write(err.__str__().encode())
         return
 
-def _run_cmd(repo_name, tag):
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    full_cmd = f'{script_dir}/onbuild.sh {repo_name} {tag}'
-    return subprocess.check_output(
-        full_cmd.split(' '),
-        stderr=subprocess.STDOUT,
-        shell=True,
-        executable='/bin/bash')
+def _run_onbuild(repo_name, tag):
+    try:
+        if not LOCK_ONBUILD.acquire(False):
+            raise ValueError('There is already an onbuild running')
+        print('entered RUNNING_ONBUILD')
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        full_cmd = f'{script_dir}/onbuild.sh {repo_name} {tag}'
+        return subprocess.check_output(
+            full_cmd.split(' '),
+            stderr=subprocess.STDOUT,
+            shell=True,
+            executable='/bin/bash')
+    finally:
+        LOCK_ONBUILD.release()
+
+def _run_analysis(unit=None, prefix=None, conc='6', fmt=None, echo=False):
+    # full_cmd = 'docker images'
+    full_cmd = f'docker run --rm hmalphettes/s3-storage-analyser'
+    if fmt is not None:
+        full_cmd += f' --fmt {fmt}'
+    if unit is not None:
+        full_cmd += f' --unit {unit}'
+    if prefix is not None:
+        full_cmd += f' --prefix {prefix}'
+    if conc is not None:
+        full_cmd += f' --conc {conc}'
+    print(full_cmd)
+    if echo:
+        return full_cmd.encode()
+    try:
+        print('entered RUNNING_ANALYSIS')
+        if not LOCK_ANALYSIS.acquire(False):
+            raise ValueError('There is already an analysis running')
+        return subprocess.check_output(
+            full_cmd,
+            stderr=subprocess.STDOUT,
+            shell=True,
+            executable='/bin/bash')
+    finally:
+        LOCK_ANALYSIS.release()
 
 if __name__ == '__main__':
     SERVER = HTTPServer(('localhost', 8000), RequestHandler)
